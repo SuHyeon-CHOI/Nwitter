6cd9b9a9a80fbe3ba06de9eb4789da26edeeefa5
SuHyeon-CHOI <tngus4869@gmail.com>
2022-08-11 10:12:32 +0900

    Updates

---
+++ precache-manifest.84b6ee489a2dd16c2a9d26873675d648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9800b9c60cc2fffa483509a27bc3f117",
+    "url": "/Nwitter/index.html"
+  },
+  {
+    "revision": "eb278f74b21d4ffd361c",
+    "url": "/Nwitter/static/css/main.55bfdf87.chunk.css"
+  },
+  {
+    "revision": "e7f062be041afef49813",
+    "url": "/Nwitter/static/js/2.f6148cf5.chunk.js"
+  },
+  {
+    "revision": "34981cc0942697370fc9d635062a9d94",
+    "url": "/Nwitter/static/js/2.f6148cf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb278f74b21d4ffd361c",
+    "url": "/Nwitter/static/js/main.50144452.chunk.js"
+  },
+  {
+    "revision": "06c4033b76c035e967b8",
+    "url": "/Nwitter/static/js/runtime-main.fcdd3e8e.js"
+  }
+]);
